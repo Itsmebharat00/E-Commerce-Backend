804a123211d8b65cb35d2025450cac5323378dd3
Bharat Singh <Bharats328@gmail.com>
2025-12-13 00:54:02 +0530

    Product Id route added

--- index.js
+++ index.js
@@ -207,6 +207,29 @@ app.get("/products/category/:categoryName", async (req, res) => {
   }
 });
 
+async function readProductById(id) {
+  try {
+    const products = await ProductSchema.findById(id);
+    return products;
+  } catch (error) {
+    console.log("Error in fetching Product by title:", error);
+    throw error;
+  }
+}
+
+app.get("/products/:productId", async (req, res) => {
+  try {
+    const products = await readProductById(req.params.productId);
+    if (products) {
+      res.status(201).json({ message: "Product Found", products });
+    } else {
+      res.status(404).json({ error: "Product not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch product by ID" });
+  }
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
